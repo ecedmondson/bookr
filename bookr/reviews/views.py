8f833b97b4fccc21f4aa63c5e36ca7dd4e6ddd03
Emily Edmondson <ecedmondson@gmail.com>
2022-02-04 21:04:52 -0600

    [Chapter 1] view for the template in the previous commit
    also required by the text. currently defaults to an empty list for
    the book list because the search can't actually do anything related to the backend.

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def fake_db_query_with_many(*args):
+    return []
+
+
 def index(request):
     name = request.GET.get("name") or "world"
     return render(request, "base.html", {"name": name})
@@ -13,3 +17,11 @@ def index_bad_or(request):
     # the commented out one is the correct one
     # name = request.GET.get("name") or "world"
     return HttpResponse(f"Hello {name}")
+
+
+def search(request):
+    search = request.GET.get("search") or None
+    book_list = fake_db_query_with_many(search)
+    return render(
+        request, "book-search.html", {"search": search, "book_list": book_list}
+    )
