27b4c60af3d4c9092d6ea75bba694c72d48cc923
Emily Edmondson <ecedmondson@gmail.com>
2022-02-04 18:57:10 -0600

    [Chapter 1] deprecate the old index
    book is going to have me delete this so i wanna save my learning this way

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    name = request.GET.get("name") or "world"
-    return HttpResponse(f"Hello {name}")
-
-
 def index_bad_or(request):
     # Breaks front end with empty string. Just becomes hello
     name = request.GET.get("name", "world")
+    # the commented out one is the correct one
+    # name = request.GET.get("name") or "world"
     return HttpResponse(f"Hello {name}")
