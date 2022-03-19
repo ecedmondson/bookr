1698c589b8a594daef0fa5f8eb04a8adce750e8a
Emily Edmondson <ecedmondson@gmail.com>
2022-03-18 23:10:43 -0500

    Chapter 5: making a note for myself

---
+++ bookr/bookr/__init__.py
@@ -0,0 +1,6 @@
+# this second bookr is the project root
+# this comment is here because i'm learning django
+# and got confused at first about the different
+# bookr_django -> book/learning root
+# bookr (first nest) -> what would be normally root in a real django project
+# bookr (second nest, where this __init__ file lives) project root
